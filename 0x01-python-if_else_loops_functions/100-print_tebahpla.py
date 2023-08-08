6ff66599419b0249937001e40356bef3a50c5820
Eric Onasis <ericonasis@gmail.com>
2023-08-09 00:12:26 +0300

    14. Smile in the mirror

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for letter in reversed (range(97, 123)):
+    if letter % 2 == 0: 
+        print("{}".format(chr(letter)), end='')
+    else:
+        (letter) -= 32
+        print("{}".format(chr(letter)), end='')
