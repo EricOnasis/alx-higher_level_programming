880ad6228347955bff097ca30e9c7ef108e94256
Eric Onasis <ericonasis@gmail.com>
2023-08-11 04:24:21 +0300

    Write a program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,29 +4,21 @@ from calculator_1 import add, sub, mul, div
 
 
 if __name__ == "__main__":
-
-    argument_count = len(argv)
-
-    if argument_count != 4:
+    argc = len(argv) - 1
+    if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(-1)
-
+        exit(1)
+    operator = argv[2]
     a = int(argv[1])
     b = int(argv[3])
-    operator = argv[2]
-
     if operator == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
-
     elif operator == "-":
         print("{} - {} = {}".format(a, b, sub(a, b)))
-
-    elif operator == " * ":
+    elif operator == "*":
         print("{} * {} = {}".format(a, b, mul(a, b)))
-
     elif operator == "/":
         print("{} / {} = {}".format(a, b, div(a, b)))
-
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
