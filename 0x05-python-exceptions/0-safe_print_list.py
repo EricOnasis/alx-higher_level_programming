dbf2570f5ee36e0fa2e4491590ace801c0b6225c
Eric Onasis <ericonasis@gmail.com>
2023-08-28 23:13:02 +0300

    Fix pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,12 +6,10 @@ def safe_print_list(my_list=[], x=0):
 
     for index in range(x):
         try:
-            print("{}".format(my_list[index]), end = "")
+            print("{}".format(my_list[index]), end="")
             count += 1
 
         except IndexError:
             break
     print("")
-    return(count)
-
-
+    return (count)
