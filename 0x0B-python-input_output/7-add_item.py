160b21b246116a3682c0eeb1020d7de033cd53a2
Eric Onasis <ericonasis@gmail.com>
2023-09-13 05:55:17 +0300

    Fix task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python3
+"""add item"""
 
-"""
-A module that contains a function that adds
-all arguments to a python list.
-"""
 
+import json
 import sys
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+import os.path
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-def add_item():
-
-    try:
-        items = load_from_json_file('add_item.json')
-    except FileNotFoundError:
-        items = []
-
-    items.extend(sys.argv[1:])
-
-    save_to_json_file(items, 'add_item.json')
-
-
-if __name__ == '__main__':
-    add_item()
+filename = "add_item.json"
+if os.path.isfile(filename):
+    obj = load_from_json_file(filename)
+else:
+    obj = []
+obj.extend(sys.argv[1:])
+save_to_json_file(obj, filename)
