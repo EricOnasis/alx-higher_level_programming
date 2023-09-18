4ab3d0a80473c2acfba6815e9672b8813aa7ea43
Eric Onasis <ericonasis@gmail.com>
2023-09-19 00:06:34 +0300

    Add dict method for Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -46,3 +46,11 @@ class Square(Rectangle):
             self.y = args[3]
         except IndexError:
             pass
+
+    def to_dictionary(self):
+        """
+            Dictionary representation of a Square instance"""
+        return {'id': getattr(self, "id"),
+                'size': getattr(self, "width"),
+                'x': getattr(self, "x"),
+                'y': getattr(self, "y")}
