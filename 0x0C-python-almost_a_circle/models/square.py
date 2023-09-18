c07d0f9ba22f8d8a4d9b99a15cf8b7198acc451b
Eric Onasis <ericonasis@gmail.com>
2023-09-18 03:59:18 +0300

    Write the class Square that inherits from Rectangle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""Class Square definition"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Class initialization"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Size of the square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.width = value
+        self.height = value
