ea0ec0e31fd37e198cfc075d5aea803e08429977
Eric Onasis <ericonasis@gmail.com>
2023-11-20 01:26:01 +0300

    Test Var

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,9 +15,9 @@ if __name__ == '__main__':
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states \
-                 WHERE name LIKE BINARY 'N%' \
-                 ORDER BY states.id ASC")
+    querry = "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+                 ORDER BY states.id ASC"
+    cur.execute(querry)
     rows = cur.fetchall()
 
     for row in rows:
