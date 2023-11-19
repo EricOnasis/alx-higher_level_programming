820547fe27aeaa8470a93528be94b1033820df61
Eric Onasis <ericonasis@gmail.com>
2023-11-20 01:23:56 +0300

    Test Var

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -15,7 +15,8 @@ if __name__ == '__main__':
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    querry = "SELECT * FROM states"
+    cur.execute(querry)
     rows = cur.fetchall()
 
     for row in rows:
